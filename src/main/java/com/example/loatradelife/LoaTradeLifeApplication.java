d1a8f48f8c2a24e4d2d0080842ebe622e4b7c47c
so_jaewon <soccat96@gmail.com>
2023-08-03 10:00:51 +0900

    first commit

---
+++ src/main/java/com/example/loatradelife/LoaTradeLifeApplication.java
@@ -0,0 +1,12 @@
+package com.example.loatradelife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoaTradeLifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoaTradeLifeApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/loatradelife/LoaTradeLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loatradelife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoaTradeLifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
