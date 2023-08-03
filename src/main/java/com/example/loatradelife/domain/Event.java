6967c680369cdbf19b3438a38263dff96f63fa93
so_jaewon <soccat96@gmail.com>
2023-08-03 15:58:35 +0900

    Create Event Entity
    Event
    EventTest

---
+++ src/main/java/com/example/loatradelife/domain/Event.java
@@ -0,0 +1,42 @@
+package com.example.loatradelife.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "event_id")
+    private Long id;
+
+    private String title;
+    private String thumbnail;
+    private String link;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private LocalDateTime rewardDate;
+
+    public Event(String title, String thumbnail, String link, LocalDateTime startDate, LocalDateTime endDate, LocalDateTime rewardDate) {
+        this.title = title;
+        this.thumbnail = thumbnail;
+        this.link = link;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.rewardDate = rewardDate;
+    }
+
+    public void updateEvent(Event event) {
+        this.title = event.getTitle();
+        this.thumbnail = event.getThumbnail();
+        this.link = event.getLink();
+        this.startDate = event.getStartDate();
+        this.endDate = event.getEndDate();
+        this.rewardDate = event.getRewardDate();
+    }
+}
---
+++ src/test/java/com/example/loatradelife/domain/EventTest.java
@@ -0,0 +1,54 @@
+package com.example.loatradelife.domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+class EventTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    public void updateEvent() {
+        LocalDateTime now = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0).withNano(0);
+        Event event = new Event(
+                "event title",
+                "event thumbnail",
+                "event link",
+                now,
+                now.plusMonths(1),
+                now.plusMonths(1).plusWeeks(1)
+        );
+        LocalDateTime newNow = now.plusDays(1);
+        Event newEvent = new Event(
+                "new title",
+                "new thumbnail",
+                "new link",
+                newNow,
+                newNow.plusMonths(1),
+                newNow.plusMonths(1).plusWeeks(1)
+        );
+
+        event.updateEvent(newEvent);
+
+        assertThat(event.getId()).isNull();
+        assertThat(event.getTitle()).isEqualTo(newEvent.getTitle());
+        assertThat(event.getThumbnail()).isEqualTo(newEvent.getThumbnail());
+        assertThat(event.getLink()).isEqualTo(newEvent.getLink());
+        assertThat(event.getStartDate()).isEqualTo(newEvent.getStartDate());
+        assertThat(event.getEndDate()).isEqualTo(newEvent.getEndDate());
+        assertThat(event.getRewardDate()).isEqualTo(newEvent.getRewardDate());
+    }
+}
