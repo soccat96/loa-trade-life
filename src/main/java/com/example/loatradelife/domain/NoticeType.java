b23bf2ea84b82cb7a22ce7a35ba2b22dc1f0e637
so_jaewon <soccat96@gmail.com>
2023-08-04 10:11:30 +0900

    Modify NoticeType
    open api의 한글 문자열을 입력 받아 enum 타입 리턴

--- src/main/java/com/example/loatradelife/domain/NoticeType.java
+++ src/main/java/com/example/loatradelife/domain/NoticeType.java
@@ -1,5 +1,16 @@
 package com.example.loatradelife.domain;
 
 public enum NoticeType {
-    NOTICE, INSPECTION, SHOP, EVENT
+    NOTICE, INSPECTION, SHOP, EVENT;
+
+    public static NoticeType getNoticeType(String type) {
+        switch (type) {
+            case "공지"   -> { return NoticeType.NOTICE; }
+            case "점검"   -> { return NoticeType.INSPECTION; }
+            case "상점"   -> { return NoticeType.SHOP; }
+            case "이벤트" -> { return NoticeType.EVENT; }
+        }
+
+        return NoticeType.NOTICE;
+    }
 }
