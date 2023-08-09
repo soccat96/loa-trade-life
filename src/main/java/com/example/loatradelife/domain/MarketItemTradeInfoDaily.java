ff59f3969904590097257b68ba211d46348be64e
so_jaewon <soccat96@gmail.com>
2023-08-09 18:32:29 +0900

    Create MarketItemTradeInfoDaily Entity
    MarketItemTradeInfoDaily
    MarketItemTradeInfoDailyTest

---
+++ src/main/java/com/example/loatradelife/domain/MarketItemTradeInfoDaily.java
@@ -0,0 +1,41 @@
+package com.example.loatradelife.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MarketItemTradeInfoDaily {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "market_item_trade_info_daily_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "code")
+    private MarketItem marketItem;
+    private LocalDateTime date;
+    private Double avgPrice;
+    private Integer tradeCount;
+
+    @Builder
+    public MarketItemTradeInfoDaily(MarketItem marketItem, LocalDateTime date, Double avgPrice, Integer tradeCount) {
+        this.marketItem = marketItem;
+        this.date = date;
+        this.avgPrice = avgPrice;
+        this.tradeCount = tradeCount;
+    }
+
+    public void updateMarketItemTradeInfoDaily(MarketItemTradeInfoDaily marketItemTradeInfoDaily) {
+        this.marketItem = marketItemTradeInfoDaily.getMarketItem();
+        this.date = marketItemTradeInfoDaily.getDate();
+        this.avgPrice = marketItemTradeInfoDaily.getAvgPrice();
+        this.tradeCount = marketItemTradeInfoDaily.getTradeCount();
+    }
+}
---
+++ src/test/java/com/example/loatradelife/domain/MarketItemTradeInfoDailyTest.java
@@ -0,0 +1,85 @@
+package com.example.loatradelife.domain;
+
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class MarketItemTradeInfoDailyTest {
+    @Autowired
+    private EntityManager entityManager;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void updateMarketItemTradeInfoDaily() {
+        LocalDateTime now = LocalDateTime.now();
+        ItemGrade normal = new ItemGrade(0,"일반");
+        ItemGrade uncommon = new ItemGrade(1,"고급");
+        entityManager.persist(normal);
+        entityManager.persist(uncommon);
+        MarketItem marketItem = new MarketItem(
+                90200,
+                68447,
+                "name",
+                2,
+                normal,
+                "link"
+        );
+        MarketItem newMarketItem = new MarketItem(
+                90200,
+                68448,
+                "name_new",
+                3,
+                uncommon,
+                "link_new"
+        );
+        entityManager.persist(marketItem);
+        entityManager.persist(newMarketItem);
+        MarketItemTradeInfoDaily marketItemTradeInfoDaily = MarketItemTradeInfoDaily.builder()
+                .marketItem(marketItem)
+                .date(now)
+                .avgPrice(16.9)
+                .tradeCount(1_000_500)
+                .build();
+        entityManager.persist(marketItemTradeInfoDaily);
+
+        MarketItemTradeInfoDaily newOne = new MarketItemTradeInfoDaily(
+                newMarketItem,
+                now.plusDays(1),
+                17.9,
+                1_000_600
+        );
+        MarketItemTradeInfoDaily.builder()
+                .marketItem(newMarketItem)
+                .date(now.plusDays(1))
+                .avgPrice(17.9)
+                .tradeCount(1_000_600)
+                .build();
+        marketItemTradeInfoDaily.updateMarketItemTradeInfoDaily(newOne);
+        entityManager.flush();
+
+        MarketItemTradeInfoDaily findById = entityManager.find(MarketItemTradeInfoDaily.class, marketItemTradeInfoDaily.getId());
+        assertThat(findById).isNotNull();
+        assertThat(findById.getMarketItem().getItemGrade().getId()).isEqualTo(uncommon.getId());
+        assertThat(findById.getMarketItem().getId()).isEqualTo(newMarketItem.getId());
+        assertThat(findById.getDate().isEqual(now.plusDays(1))).isTrue();
+        assertThat(findById.getAvgPrice()).isEqualTo(17.9);
+        assertThat(findById.getTradeCount()).isEqualTo(1_000_600);
+    }
+}
