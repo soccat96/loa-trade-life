c2f34d03a04d91d25008627a12065a174fbb403f
so_jaewon <soccat96@gmail.com>
2023-09-08 03:19:14 +0900

    Modify EventRepository
    event 목록을 시작일과 끝일을 기준으로 가져오는 메소드 추가

--- src/main/java/com/example/loatradelife/repository/EventRepository.java
+++ src/main/java/com/example/loatradelife/repository/EventRepository.java
@@ -3,6 +3,11 @@ package com.example.loatradelife.repository;
 import com.example.loatradelife.domain.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface EventRepository extends JpaRepository<Event, Long> {
     boolean existsEventByTitle(String title);
+
+    List<Event> findByStartDateBetweenOrderByStartDate(LocalDateTime startDate, LocalDateTime endDate);
 }
