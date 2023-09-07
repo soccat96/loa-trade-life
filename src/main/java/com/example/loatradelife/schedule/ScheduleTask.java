1b3c8dd96ebd0dccbf5d5c639ec2c479acca258b
so_jaewon <soccat96@gmail.com>
2023-09-07 11:43:59 +0900

    Modify ScheduleTask
    코드 리팩토링
    http url connection 공통 부분 추출
    커넥션을 Java object 로 변환하는 부분 추출
    로깅 시작부분, 끝 부분 추출

--- src/main/java/com/example/loatradelife/schedule/ScheduleTask.java
+++ src/main/java/com/example/loatradelife/schedule/ScheduleTask.java
@@ -33,127 +33,125 @@ public class ScheduleTask {
     private final MarketItemService marketItemService;
     private final MarketItemTradeInfoDailyService marketItemTradeInfoDailyService;
 
+    private void loggingStart(String methodName) {
+        log.info("start " + methodName + "::==" + LocalDateTime.now());
+    }
+
+    private void loggingEnd(String methodName, int addedEventCount) {
+        log.info("added event count::==" + addedEventCount);
+        log.info("end " + methodName + "::==" + LocalDateTime.now());
+    }
+
+    private HttpURLConnection getHttpURLConnection(String url) {
+        HttpURLConnection connection;
+        try {
+            connection = (HttpURLConnection) new URL(lostArkOpenApiConfig.getBaseUrl() + url).openConnection();
+            connection.setRequestProperty("authorization", "bearer " + lostArkOpenApiConfig.getKey());
+            connection.setRequestProperty("accept", "application/json");
+            connection.setRequestMethod("GET");
+            connection.connect();
+
+            return connection;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+            // connection exception
+        }
+    }
+
+    private List<Map<String, Object>> getMapList(HttpURLConnection connection) {
+        ObjectMapper ob = new ObjectMapper();
+        try {
+            return ob.readValue(connection.getInputStream(), new TypeReference<>() {});
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+            // data transfer exception
+        }
+    }
+
     // second minute hour date month dayOfWeek
     @Scheduled(cron = "0 */1 * * * *")
     public void getEvents() {
-        log.info("start 'getEvents()'::==" + LocalDateTime.now());
+        loggingStart("getEvents()");
+
         int addedEventCount = 0;
+        List<Map<String, Object>> mapList = getMapList(getHttpURLConnection("/news/events"));
+        for (Map<String, Object> m : mapList) {
+            Event event = new Event(
+                    (String) m.get("Title"),
+                    (String) m.get("Thumbnail"),
+                    (String) m.get("Link"),
+                    LocalDateTime.parse((String )m.get("StartDate"), DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+                    LocalDateTime.parse((String) m.get("EndDate"), DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+                    m.get("RewardDate") == null
+                            ? null
+                            : LocalDateTime.parse((String) m.get("RewardDate"), DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+            );
 
-        try {
-            HttpURLConnection connection = (HttpURLConnection) new URL(lostArkOpenApiConfig.getBaseUrl() + "/news/events").openConnection();
-            connection.setRequestProperty("authorization", "bearer " + lostArkOpenApiConfig.getKey());
-            connection.setRequestProperty("accept", "application/json");
-            connection.setRequestMethod("GET");
-            connection.connect();
-
-            ObjectMapper ob = new ObjectMapper();
-            List<Map<String, String>> mapList = ob.readValue(connection.getInputStream(), new TypeReference<>() {});
-
-            for (Map<String, String> m : mapList) {
-                Event event = new Event(
-                        m.get("Title"),
-                        m.get("Thumbnail"),
-                        m.get("Link"),
-                        LocalDateTime.parse(m.get("StartDate"), DateTimeFormatter.ISO_LOCAL_DATE_TIME),
-                        LocalDateTime.parse(m.get("EndDate"), DateTimeFormatter.ISO_LOCAL_DATE_TIME),
-                        m.get("RewardDate") == null
-                                ? null
-                                : LocalDateTime.parse(m.get("RewardDate"), DateTimeFormatter.ISO_LOCAL_DATE_TIME)
-                );
-
-                Long result = eventService.saveEvent(event);
-                if (result > 0) {
-                    addedEventCount++;
-                }
+            Long result = eventService.saveEvent(event);
+            if (result > 0) {
+                addedEventCount++;
             }
-
-            log.info("added event count::==" + addedEventCount);
-            log.info("end 'getEvents()'::==" + LocalDateTime.now());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
+
+        loggingEnd("getEvents()", addedEventCount);
     }
 
     // second minute hour date month dayOfWeek
     @Scheduled(cron = "0 */1 * * * *")
-    public void getNotice() {
-        log.info("start 'getNotice()::==" + LocalDateTime.now());
+    public void getNotices() {
+        loggingStart("getNotices()");
+
         int addedNoticeCount = 0;
+        List<Map<String, Object>> mapList = getMapList(getHttpURLConnection("/news/notices"));
+        for (Map<String, Object> x : mapList) {
+            Notice notice = new Notice(
+                    (String) x.get("Title"),
+                    LocalDateTime.parse((String) x.get("Date"), DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+                    (String) x.get("Link"),
+                    NoticeType.getNoticeType((String) x.get("Type"))
+            );
 
-        try {
-            HttpURLConnection connection = (HttpURLConnection) new URL(lostArkOpenApiConfig.getBaseUrl() + "/news/notices").openConnection();
-            connection.setRequestProperty("authorization", "bearer " + lostArkOpenApiConfig.getKey());
-            connection.setRequestProperty("accept", "application/json");
-            connection.setRequestMethod("GET");
-            connection.connect();
-
-            ObjectMapper ob = new ObjectMapper();
-            List<Map<String, String>> mapList = ob.readValue(connection.getInputStream(), new TypeReference<>() {});
-            for (Map<String, String> x : mapList) {
-                Notice notice = new Notice(
-                        x.get("Title"),
-                        LocalDateTime.parse(x.get("Date"), DateTimeFormatter.ISO_LOCAL_DATE_TIME),
-                        x.get("Link"),
-                        NoticeType.getNoticeType(x.get("Type"))
-                );
-
-                Long result = noticeService.saveNotice(notice);
-                if (result > 0) {
-                    addedNoticeCount++;
-                }
+            Long result = noticeService.saveNotice(notice);
+            if (result > 0) {
+                addedNoticeCount++;
             }
-
-            log.info("added notice count::==" + addedNoticeCount);
-            log.info("end 'getNotice()::==" + LocalDateTime.now());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
+
+        loggingEnd("getNotices()", addedNoticeCount);
     }
 
     @Scheduled(cron = "0 */1 * * * *")
-    public void getMarketItemTradeInfoDaily() {
-        log.info("start 'getMarketItemTradeInfoDaily()::==" + LocalDateTime.now());
-        int addedCount = 0;
+    public void getMarketItemTradeInfoDailies() {
+        loggingStart("getMarketItemTradeInfoDailies()");
 
+        int addedCount = 0;
         List<MarketItem> marketItemList = marketItemService.findAllMarketItem();
         for (MarketItem marketItem : marketItemList) {
-            try {
-                HttpURLConnection connection = (HttpURLConnection) new URL(lostArkOpenApiConfig.getBaseUrl() + "/markets/items/" + marketItem.getCode()).openConnection();
-                connection.setRequestProperty("authorization", "bearer " + lostArkOpenApiConfig.getKey());
-                connection.setRequestProperty("accept", "application/json");
-                connection.setRequestMethod("GET");
-                connection.connect();
+            List<Map<String, Object>> mapList = getMapList(getHttpURLConnection("/markets/items/" + marketItem.getCode()));
+            Map<String, Object> obj = mapList.get(0);
+            Integer bundleCount = (Integer) obj.get("BundleCount");
+            List<Map<String, Object>> dataList = (List<Map<String,Object>>) obj.get("Stats");
+            for (Map<String, Object> x : dataList) {
+                MarketItemTradeInfoDaily data = MarketItemTradeInfoDaily.builder()
+                        .marketItem(marketItem)
+                        .date(
+                                LocalDateTime.of(
+                                        LocalDate.parse((String) x.get("Date"), DateTimeFormatter.ISO_LOCAL_DATE),
+                                        LocalTime.of(0, 0, 0)
+                                )
+                        )
+                        .avgPrice((Double) x.get("AvgPrice"))
+                        .tradeCount((Integer) x.get("TradeCount"))
+                        .bundleCount(bundleCount)
+                        .build();
 
-                ObjectMapper ob = new ObjectMapper();
-                List<Map<String, Object>> mapList = ob.readValue(connection.getInputStream(), new TypeReference<>() {});
-                Map<String, Object> obj = mapList.get(0);
-                Integer bundleCount = (Integer) obj.get("BundleCount");
-                List<Map<String, Object>> dataList = (List<Map<String,Object>>) obj.get("Stats");
-                for (Map<String, Object> x : dataList) {
-                    MarketItemTradeInfoDaily data = MarketItemTradeInfoDaily.builder()
-                            .marketItem(marketItem)
-                            .date(
-                                    LocalDateTime.of(
-                                            LocalDate.parse((String) x.get("Date"), DateTimeFormatter.ISO_LOCAL_DATE),
-                                            LocalTime.of(0, 0, 0)
-                                    )
-                            )
-                            .avgPrice((Double) x.get("AvgPrice"))
-                            .tradeCount((Integer) x.get("TradeCount"))
-                            .bundleCount(bundleCount)
-                            .build();
-
-                    Long result = marketItemTradeInfoDailyService.saveMarketItemTradeInfoDaily(data);
-                    if (result > 0) {
-                        addedCount++;
-                    }
+                Long result = marketItemTradeInfoDailyService.saveMarketItemTradeInfoDaily(data);
+                if (result > 0) {
+                    addedCount++;
                 }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
             }
         }
 
-        log.info("added market item trade info daily count::==" + addedCount);
-        log.info("end 'getMarketItemTradeInfoDaily()::==" + LocalDateTime.now());
+        loggingEnd("getMarketItemTradeInfoDailies()", addedCount);
     }
 }
