4c54ba78fcd6e565b089e20ff9da1ffcf1de9d9d
so_jaewon <soccat96@gmail.com>
2023-09-08 14:36:43 +0900

    Modify NoticeService
    notice 목록을 시작일과 끝일을 기준으로 가져오는 메소드 추가
    NoticeServiceTest 테스트 코드 추가

--- src/main/java/com/example/loatradelife/repository/NoticeRepository.java
+++ src/main/java/com/example/loatradelife/repository/NoticeRepository.java
@@ -3,6 +3,11 @@ package com.example.loatradelife.repository;
 import com.example.loatradelife.domain.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
     boolean existsNoticeByTitle(String title);
+
+    List<Notice> findByDateBetweenOrderByDate(LocalDateTime startDate, LocalDateTime endDate);
 }
--- src/main/java/com/example/loatradelife/service/NoticeService.java
+++ src/main/java/com/example/loatradelife/service/NoticeService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,6 +33,10 @@ public class NoticeService {
 
     public Optional<Notice> findOneNotice(Long id) {
         return noticeRepository.findById(id);
+    }
+
+    public List<Notice> findListByDateBetween(LocalDateTime startDate, LocalDateTime endDate) {
+        return noticeRepository.findByDateBetweenOrderByDate(startDate, endDate);
     }
 
     @Transactional
