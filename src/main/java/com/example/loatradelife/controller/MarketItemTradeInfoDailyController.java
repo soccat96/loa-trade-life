bb205b106200a1049b4bde6ec37c5a25acee237a
so_jaewon <soccat96@gmail.com>
2023-09-15 10:33:25 +0900

    Modify MarketItemTradeInfoDailyController
    MarketItemTradeInfoDailyDto 제거
    uri 변경

--- src/main/java/com/example/loatradelife/controller/MarketItemTradeInfoDailyController.java
+++ src/main/java/com/example/loatradelife/controller/MarketItemTradeInfoDailyController.java
@@ -1,12 +1,8 @@
 package com.example.loatradelife.controller;
 
-import com.example.loatradelife.domain.ItemGrade;
-import com.example.loatradelife.domain.MarketItem;
+import com.example.loatradelife.controller.dto.MarketItemTradeInfoDailyDto;
 import com.example.loatradelife.domain.MarketItemTradeInfoDaily;
 import com.example.loatradelife.service.MarketItemTradeInfoDailyService;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,7 +13,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1/market-item-trade-info-daily")
+@RequestMapping("/api/v1/market-item-trade-info-dailies")
 @RequiredArgsConstructor
 public class MarketItemTradeInfoDailyController {
     private final MarketItemTradeInfoDailyService marketItemTradeInfoDailyService;
@@ -42,45 +38,5 @@ public class MarketItemTradeInfoDailyController {
     public Result<MarketItemTradeInfoDailyDto> getMarketItemTradeInfoDaily(@PathVariable(name = "id") String id) {
         Optional<MarketItemTradeInfoDaily> data = marketItemTradeInfoDailyService.findOneMarketItemTradeInfoDaily(Long.parseLong(id));
         return data.map(marketItemTradeInfoDaily -> new Result<>(new MarketItemTradeInfoDailyDto(marketItemTradeInfoDaily))).orElseGet(() -> new Result<>(new MarketItemTradeInfoDailyDto()));
-    }
-
-    @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    static class MarketItemTradeInfoDailyDto {
-        private Integer number;
-        private String itemGradeName;
-
-        private Integer categoryCode;
-        private Integer code;
-        private String marketItemName;
-        private Integer tier;
-        private String imageLink;
-
-        private Long id;
-        private LocalDateTime date;
-        private Double avgPrice;
-        private Integer tradeCount;
-        private Integer bundleCount;
-
-        public MarketItemTradeInfoDailyDto(MarketItemTradeInfoDaily marketItemTradeInfoDaily) {
-            MarketItem marketItem = marketItemTradeInfoDaily.getMarketItem();
-            ItemGrade itemGrade = marketItem.getItemGrade();
-
-            this.number = itemGrade.getNumber();
-            this.itemGradeName = itemGrade.getName();
-
-            this.categoryCode = marketItem.getCategoryCode();
-            this.code = marketItem.getCode();
-            this.marketItemName = marketItem.getName();
-            this.tier = marketItem.getTier();
-            this.imageLink = marketItem.getImageLink();
-
-            this.id = marketItemTradeInfoDaily.getId();
-            this.date = marketItemTradeInfoDaily.getDate();
-            this.avgPrice = marketItemTradeInfoDaily.getAvgPrice();
-            this.tradeCount = marketItemTradeInfoDaily.getTradeCount();
-            this.bundleCount = marketItemTradeInfoDaily.getBundleCount();
-        }
     }
 }
